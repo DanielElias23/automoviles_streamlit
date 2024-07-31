b5171b36c2029bc5cce0efef32be93e21a159f57
DanielElias23 <danielchingasilva@gmail.com>
2024-07-31 10:43:15 -0400

    nada

--- pages/Predictor_de_precios__de_autos.py
+++ pages/Predictor_de_precios__de_autos.py
@@ -8,7 +8,10 @@ import streamlit as st
 data = pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-ML240EN-SkillsNetwork/labs/data/CarPrice_Assignment.csv')
 
 st.header("Predictor de precios de automoviles")
-st.write("Seleccione las caracteriticas del modelo de automovil que quiere predecir el precio estimado en el mercado, luego presione enviar.")
+#st.write("Seleccione las caracteriticas del modelo de automovil que quiere predecir el precio estimado en el mercado, luego presione enviar.")
+
+st.write("Lo que hace el predictor, es que segun las caracterisitcas de cierto vehiculo puede predecir cual seria el precio en el mercado de ese vehiculo.")
+st.write("El predictor de precios de vehiculos se utiliza para saber si una empresa automotriz puede entrar a un nuevo mercado y si el negocio puede ser viable, al ya conocer a cuanto podria ofrecer los vehiculos, en el caso de ya contar con una empresa automotriz se puede utilizar para saber a cuanto se podria ofrecer un nuevo modelo de vehiculo.")
 #st.write("""
 #          Una empresa de automoviles pretender entrar al mercado de un pais, para ello necesita saber a cuanto podria vender sus automoviles
 #          para saber si es viable el negocio, entonces hace un estudio para saber el precio de los automoviles que ofrece el mercado con sus
@@ -145,7 +148,7 @@ if variable_input_sumit:
         
         y_pred_sol = pipe_en.predict(data_2)
         st.header("El precio estimado para este vehiculo es:")
-        st.title(f"{round(float(y_pred_sol),2)}$")
+        st.title(f"{round(float(y_pred_sol),2)} $")
         
         
         """Modelo ML: 
