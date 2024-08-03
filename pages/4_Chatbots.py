bc1594f86fc89133950937c405b913b17ed9f325
DanielElias23 <danielchingasilva@gmail.com>
2024-08-03 05:52:36 -0400

    nada

--- pages/4_Chatbots.py
+++ pages/4_Chatbots.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_community.llms import Ollama
 from langchain_core.messages import HumanMessage, AIMessage
-from langchain_core.prompts import ChatPromptTemplate, MEssagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 st.write(":blue[No implementado aun, se implementará los proximos días]")
 
 
