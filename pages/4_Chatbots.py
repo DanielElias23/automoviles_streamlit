f392f4d289fcaab6d1b70c624b444b2add7d81bc
DanielElias23 <danielchingasilva@gmail.com>
2024-08-04 11:09:44 -0400

    nada

--- pages/4_Chatbots.py
+++ pages/4_Chatbots.py
@@ -82,21 +82,21 @@ with pagina2:
     #if numero2 =="Muy creativo":
     #  numero2=2
     cleint = Groq(api_key="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")
-    def get_ai_response2(messages2):
+    def get_ai_response(messages):
       completion = client.chat.completions.create(
               model="gemma2-9b-it",
-              messages2=messages2,
+              messages=messages,
               temperature=1,  #0.7,
               max_tokens=1024,
               stream=True,
       )
       
-      response2 = "".join(chunk.choices[0].delta.content or "" for chunk in completion)
-      return response2
+      response = "".join(chunk.choices[0].delta.content or "" for chunk in completion)
+      return response
       
 
       
-    def chat2():
+    def chat():
       st.title("Chat con Gemma 2")
       st.write("¡Bienvenidos al chat con IA! Para refrescar la conversación actualiza la página.")
       if "messages" not in st.session_state:
@@ -107,33 +107,33 @@ with pagina2:
       
       
       
-      def submit2():
-            user_input2 = st.session_state.user_input2
-            if user_input2.lower() == "exit":
+      def submit():
+            user_input = st.session_state.user_input
+            if user_input.lower() == "exit":
                   st.write("!Gracias por chatear! ¡Adios!")
                   st.stop()
             #if i in numero:                  
             
-            st.session_state["messages2"].append({"role2": "user2", "content2": user_input2})
+            st.session_state["messages"].append({"role": "user", "content": user_input})
             
             with st.spinner("Obtieniendo respuesta..."):
-                 ai_response2 = get_ai_response2(st.session_state["messages2"])
-                 st.session_state["messages2"].append({"role2": "assistant2", "content2": ai_response2})  
+                 ai_response = get_ai_response(st.session_state["messages"])
+                 st.session_state["messages"].append({"role": "assistant", "content": ai_response})  
                  
             st.session_state.user_input2 = ""
             
             
-      for message2 in st.session_state["messages2"]:
-            role2 = "- 👨 **Tu** " if message2["role2"] == "user2" else "- 🤖 **Bot**"
-            st.write(f"{role2}: {message2['content2']}")
+      for message2 in st.session_state["messages"]:
+            role = "- 👨 **Tu** " if message["role"] == "user" else "- 🤖 **Bot**"
+            st.write(f"{role}: {message['content']}")
             
       with st.form(key="chat_form2", clear_on_submit=True):
-            st.text_input("Tu:", key="user_input2")
-            submit_button2 = st.form_submit_button(label="Enviar", on_click=submit2)
+            st.text_input("Tu:", key="user_input")
+            submit_button = st.form_submit_button(label="Enviar", on_click=submit)
             
       
     if __name__ == "__main__":
-      chat2()      
+      chat()      
       
       
       
