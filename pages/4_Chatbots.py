f7de604fc5f76552a1fcd983c1dc743562dc600c
DanielElias23 <danielchingasilva@gmail.com>
2024-08-04 11:07:27 -0400

    nada

--- pages/4_Chatbots.py
+++ pages/4_Chatbots.py
@@ -83,9 +83,9 @@ with pagina2:
     #  numero2=2
     cleint = Groq(api_key="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")
     def get_ai_response2(messages2):
-      completion = client.chat2.completions.create(
+      completion = client.chat.completions.create(
               model="gemma2-9b-it",
-              messages=messages2,
+              messages2=messages2,
               temperature=1,  #0.7,
               max_tokens=1024,
               stream=True,
