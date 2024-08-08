596732264547758118efed1bc32c7f5d97604f15
DanielElias23 <danielchingasilva@gmail.com>
2024-08-08 15:33:49 -0400

    nada

--- pages/2_Perdida_de_clientes.py
+++ pages/2_Perdida_de_clientes.py
@@ -7,6 +7,63 @@ import requests
 from PIL import Image
 import urllib.request
 
+import base64
+import streamlit as st
+
+
+
+@st.cache_data
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+#"https://images.unsplash.com/photo-1501426026826-31c667bdf23d"
+#data:image/png;base64,{img}
+img = get_img_as_base64("de_chat.png")
+
+#https://www.blogdelfotografo.com/wp-content/uploads/2021/12/Fondo_Negro_3.webp
+#https://w0.peakpx.com/wallpaper/596/336/HD-wallpaper-azul-marino-agua-clima-nubes-oscuro-profundo.jpg
+#
+#https://i.pinimg.com/564x/8b/5a/a4/8b5aa4783578968cd257b0b5418f3645.jpg
+#https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjkwNy1hdW0tNDQteC5qcGc.jpg
+#https://i.pinimg.com/736x/f6/c1/0a/f6c10a01b8f7c3285fc660b0b0664e52.jpg
+#https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjkwNy1hdW0tNDQteC5qcGc.jpg
+#https://c.wallhere.com/photos/ac/a0/color_pattern_texture_background_dark-672126.jpg!d
+
+#Colores
+#https://wallpapers.com/images/hd/dark-blue-plain-thxhzarho60j4alk.jpg
+
+#https://e0.pxfuel.com/wallpapers/967/154/desktop-wallpaper-solid-black-1920%C3%971080-black-solid-color-background-top.jpg
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://www.colorhexa.com/191b20.png");
+background-size: 100%;
+background-position: top right;
+background-repeat: repeat;
+background-attachment: local;
+}}
+
+[data-testid="stSidebar"] > div:first-child {{
+background-image: url("https://wallpapers.com/images/hd/dark-blue-plain-thxhzarho60j4alk.jpg");
+background-size: 30%;
+background-position: top left; 
+background-repeat: no-repeat;
+background-attachment: fixed;
+}}
+
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 
 churn_df = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/ChurnData.csv")
 f1_knn=Image.open("f1_knn.png")
