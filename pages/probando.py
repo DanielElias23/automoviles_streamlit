96e7822a9cfb3575fa3df475f1fe790cc770b5a3
DanielElias23 <danielchingasilva@gmail.com>
2024-08-17 13:44:05 -0400

    nada

--- pages/probando.py
+++ pages/probando.py
@@ -3,10 +3,12 @@ import cv2
 from ultralytics.models.yolo.detect.predict import DetectionPredictor
 import streamlit as st
 
-#img_file_buffer = st.camera_input("Take a picture", type=Video)
+img_file_buffer = st.camera_input("Take a picture", type=Video)
 
 model= YOLO("yolov8s.pt")
 
+results = model.predict(source="img_file_buffer", show=True)
+
 #results = model.predict(source="0", show=True)
 
-#st.write(results)
+st.write(results)
