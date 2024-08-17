e50360da7133597840f97ef54dee8e725da7e7bc
DanielElias23 <danielchingasilva@gmail.com>
2024-08-17 13:48:05 -0400

    nada

--- pages/probando.py
+++ pages/probando.py
@@ -3,12 +3,24 @@ import cv2
 from ultralytics.models.yolo.detect.predict import DetectionPredictor
 import streamlit as st
 
-img_file_buffer = st.camera_input("Take a picture", type=Video)
+#img_file_buffer = st.camera_input("Take a picture", type=Video)
 
 model= YOLO("yolov8s.pt")
 
-results = model.predict(source="img_file_buffer", show=True)
+#results = model.predict(source="img_file_buffer", show=True)
 
 #results = model.predict(source="0", show=True)
 
-st.write(results)
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
+
+while run:
+    _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('Stopped')
+
+#st.write(results)
