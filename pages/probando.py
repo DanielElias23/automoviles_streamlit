07f2009ef89893be054e3d30e65a13c681fb0742
DanielElias23 <danielchingasilva@gmail.com>
2024-08-17 13:01:17 -0400

    nada

--- pages/probando.py
+++ pages/probando.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
-from ultralytics.models.yolo.detect.predict import DetectionPredictor
-import cv2
-import streamlit as st
+#from ultralytics.models.yolo.detect.predict import DetectionPredictor
+#import cv2
+#import streamlit as st
 
-model= YOLO("yolov8s.pt")
+#model= YOLO("yolov8s.pt")
 
-results = model.predict(source="0", show=True)
+#results = model.predict(source="0", show=True)
 
-st.write(results)
+#st.write(results)
