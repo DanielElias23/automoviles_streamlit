921fc579defc948c5e15d6c4882c6b2bc6ee3294
DanielElias23 <danielchingasilva@gmail.com>
2024-08-18 10:51:56 -0400

    nada

--- pages/6_Reconocimiento_de_imagenes.py
+++ pages/6_Reconocimiento_de_imagenes.py
@@ -21,7 +21,7 @@ from tensorflow.keras.layers import Dropout, Flatten,Activation
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization
 import tensorflow as tf
 import random as rn
-import cv2                  
+from cv2 import imread, resize                  
 from tqdm import tqdm
 import os                   
 from random import shuffle  
